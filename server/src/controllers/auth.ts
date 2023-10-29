83142256d9b8ffa03d51c73a56917b5b5b8777e6
ba144220 <hs41511@gmail.com>
2023-10-29 19:18:09 +0800

    bcrypt and jwt

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,13 +1,16 @@
 import { db } from "../db";
 import { usersTable } from "../db/schema";
+import { env } from "../utils/env";
 import {
   SignInRequest,
   SignInResponse,
   SignUpRequest,
   SignUpResponse,
 } from "@package/types/api";
+import bcrypt from "bcrypt";
 import { eq, sql } from "drizzle-orm";
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (
   req: Request<never, never, SignUpRequest>,
@@ -31,22 +34,50 @@ export const signUp = async (
       return res.status(400).json({ error: "User already exists" });
     }
 
+    // Step 1: Hash password
+    // We use bcrypt to hash the password
+    const hashedPassword = await bcrypt.hash(password, 12);
+    // We can take a look at the hashed password
+    console.log("password", password);
+    console.log("hashedPassword", hashedPassword);
+
     // Create user
     const [user] = await db
       .insert(usersTable)
       .values({
         name,
         email,
-        hashedPassword: password,
+        // We store the hashed password in the database, so that even if the database is compromised,
+        // the attacker will not be able to see the password
+        // It is important to never store the password in plain text
+        hashedPassword: hashedPassword,
       })
       .returning();
+
+    // Step 2: Create json web token
+    // We use jwt to create a json web token
+    const token = jwt.sign(
+      {
+        userId: user.displayId,
+      },
+      env.JWT_SECRET, // JWT_SECRET is a secret string that only the server knows and should be regularly updated
+      {
+        expiresIn: env.JWT_EXPIRES_IN, // JWT_EXPIRES_IN is the duration of the token
+      },
+    );
+    // We can take a look at the token
+    console.log("token", token);
+    // The userId is contained in the token, but without the secret,
+    // others cannot decode and see the information inside the token
+    // Also, others cannot forge a token without the secret
+
     return res.status(200).json({
       user: {
         id: user.displayId,
         name: user.name,
         email: user.email,
       },
-      token: "token",
+      token: token,
     });
   } catch (error) {
     return res.status(500).json({ error: "Internal server error" });
@@ -78,9 +109,25 @@ export const signIn = async (
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
-    if (user.hashedPassword !== password) {
+
+    // Step 4: Compare password
+    // We use bcrypt to compare the password since the password in DB is hashed
+    const isPasswordValid = await bcrypt.compare(password, user.hashedPassword);
+    if (!isPasswordValid) {
       return res.status(401).json({ error: "Invalid password" });
     }
+
+    // Step 5: Create json web token
+    // Same as signUp
+    const token = jwt.sign(
+      {
+        userId: user.id,
+      },
+      env.JWT_SECRET,
+      {
+        expiresIn: env.JWT_EXPIRES_IN,
+      },
+    );
 
     return res.status(200).json({
       user: {
@@ -88,7 +135,7 @@ export const signIn = async (
         name: user.name,
         email: user.email,
       },
-      token: "token",
+      token: token,
     });
   } catch (error) {
     return res.status(500).json({ error: "Internal server error" });
--- server/src/utils/env.ts
+++ server/src/utils/env.ts
@@ -6,6 +6,8 @@ dotenv.config({ path: "./.env" });
 const envSchema = z.object({
   PORT: z.string().default("4000"),
   POSTGRES_URL: z.string().url(),
+  JWT_SECRET: z.string(),
+  JWT_EXPIRES_IN: z.string(),
 });
 
 type Env = z.infer<typeof envSchema>;
@@ -13,6 +15,8 @@ type Env = z.infer<typeof envSchema>;
 export const env: Env = {
   PORT: process.env.PORT!,
   POSTGRES_URL: process.env.POSTGRES_URL!,
+  JWT_SECRET: process.env.JWT_SECRET!,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN!,
 };
 
 envSchema.parse(env);
